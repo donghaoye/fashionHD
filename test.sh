8c9000036e6954906ab416ad7c02df07434746db
ly015 <liyining0712@gmail.com>
2018-03-04 11:30:25 +0800

    update

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-declare -a id_arr=("5.4" "6.7")
+declare -a id_arr=("5.4" "6.0" )
 
 for model_id in "${id_arr[@]}"
 do
-    python test_mmdgan_transfer_edge.py --id $model_id --gpu_ids 1
+    python test_mmdganv2_transfer_edge.py --id $model_id --gpu_ids 12
+    python test_mmdganv2_transfer_color.py --id $model_id --gpu_ids 12
 done
 
-python test_mmdgan_transfer_color.py --id 6.7 --gpu_ids 1
