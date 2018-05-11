afa77c7d9a6fb950882ec9caa6efeaa8ee45a849
ly015 <liyining0712@gmail.com>
2018-05-11 15:30:04 +0800

    update

--- options/pose_transfer_options.py
+++ options/pose_transfer_options.py
@@ -42,7 +42,7 @@ class BasePoseTransferOptions(BaseOptions):
         ##############################
         # Transformer Setting - 2stage
         ##############################
-        parser.add_argument('--which_model_stage_1', type=str, default='PoseTransfer_4.7.8', help='pretrained pose transfer model as stage-1 network')
+        parser.add_argument('--which_model_stage_1', type=str, default='PoseTransfer_4.3', help='pretrained pose transfer model as stage-1 network')
         parser.add_argument('--which_model_s2d', type=str, default='resnet', choices=['resnet', 'unet'], help='stage-2 decoder architecture')
         parser.add_argument('--s2e_nf', type=int, default=32, help='2-stage model setting: channel number of the first encoder conv layer')
         parser.add_argument('--s2e_max_nf', type=int, default=128, help='2-stage model setting: max channel number of encoder conv layers')
@@ -74,8 +74,12 @@ class BasePoseTransferOptions(BaseOptions):
         ###########################################
         # Add id profix
         ###########################################
-        if not opt.id.startswith('PoseTransfer_'):
-            opt.id = 'PoseTransfer_' + opt.id
+        if opt.which_model_T == '2stage':
+            if not opt.id.startswith('PoseTransfer_2s_'):
+                opt.id = 'PoseTransfer_2s_' + opt.id
+        else:
+            if not opt.id.startswith('PoseTransfer_'):
+                opt.id = 'PoseTransfer_' + opt.id
 
 
 class TrainPoseTransferOptions(BasePoseTransferOptions):
