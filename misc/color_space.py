f32962012c20e65691fdd475eeaae3aef5d8f4fb
ly015 <liyining0712@gmail.com>
2018-05-31 16:35:19 +0800

    update

--- misc/color_space.py
+++ misc/color_space.py
@@ -3,7 +3,6 @@ from __future__ import division
 import torch
 import numpy
 
-
 def rgb2lab(img_in):
     '''
     transformation froim RGB to Lab.
@@ -12,15 +11,25 @@ def rgb2lab(img_in):
     Output:
         img_out: (bsz, c, h, w) tensof of Lab image batch.
     '''
+    bsz, c, h, w = img_in.size()
     # convert value to [0,1]
     rgb = (img_in + 1) * 0.5
     # convert to XYZ space
-    rgb = torch.where(rgb>0.04045, torch.pow((rgb+0.055)/1.055, 2.4), rgb/12.92)
-    x = (rgb[:,0]*0.4124 + rgb[:,1]*0.3576 + rgb[:,2]*0.1805) / 0.95047
-    y = (rgb[:,0]*0.2126 + rgb[:,1]*0.7152 + rgb[:,2]*0.0722) / 1.
-    z = (rgb[:,0]*0.0193 + rgb[:,1]*0.1192 + rgb[:,2]*0.9505) / 1.08883
-    xyz = torch.stack([x,y,z], dim=1)
-    xyz = torch.where(xyz>0.008856, torch.pow(xyz, 1./3), 7.787*xyz + 16./116)
+    # rgb = torch.where(rgb>0.04045, torch.pow((rgb+0.055)/1.055, 2.4), rgb/12.92)
+    # x = (rgb[:,0]*0.4124 + rgb[:,1]*0.3576 + rgb[:,2]*0.1805) / 0.95047
+    # y = (rgb[:,0]*0.2126 + rgb[:,1]*0.7152 + rgb[:,2]*0.0722) / 1.
+    # z = (rgb[:,0]*0.0193 + rgb[:,1]*0.1192 + rgb[:,2]*0.9505) / 1.08883
+    # xyz = torch.stack([x,y,z], dim=1)
+    # xyz = torch.where(xyz>0.008856, torch.pow(xyz, 1./3), 7.787*xyz + 16./116)
+    m1 = (rgb > 0.04045).float()
+    rgb = torch.pow((rgb+0.055)/1.055, 2.4) * m1 + rgb/12.92*(1-m1)
+    M1 = [[0.4124/0.95047, 0.3576/0.95047, 0.1805/0.95047], \
+          [0.2126, 0.7152, 0.0722],\
+          [0.0193/1.08883, 0.1192/1.08883, 0.9505/1.08883]]
+    M1 = rgb.new(M1).view(1,3,3)
+    xyz = torch.matmul(M1, rgb.view(bsz, c, h*w)).view(bsz, c, h, w)
+    m2 = (xyz>0.008856).float()
+    xyz = torch.pow(xyz, 1./3)*m2 + (xyz*7.787)*(1-m2) +16./116
     # convert to Lab space
     l = 116. * xyz[:,1] - 16
     a = 500. * (xyz[:,0] - xyz[:,1])
